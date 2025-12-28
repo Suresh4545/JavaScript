a25e92c91d2617788227d899dd10ade98bd9f18d
Suresh Nishad <79903177+Suresh4545@users.noreply.github.com>
2025-12-28 09:31:33 +0000

    container

---
+++ 02_Array.js
@@ -0,0 +1,12 @@
+const myArra = [1, 2, 3, 4, 5, 6];
+//console.log(myArra);
+// const newArra = myArra.join();
+// console.log(newArra);
+console.log("A", myArra);
+const myn1 = myArra.slice(1, 3);
+
+console.log(myn1);
+console.log("B", myArra);
+const myn2 = myArra.splice(1, 3);
+console.log(myn2);
+console.log("C", myArra);
